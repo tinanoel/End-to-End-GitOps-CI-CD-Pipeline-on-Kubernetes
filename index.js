4e66b89b8ea5e9fe9633014005939f0afb8cebee
Victor Wasonga <117116140+Victorwasonga@users.noreply.github.com>
2025-05-17 14:31:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('We have successfully Deployed an Application with GitHub Actions + ArgoCD | End-to-End GitOps CI/CD Pipeline on Kubernetes');
+});
+
+app.listen(port, () => {
+  console.log(`App running on http://localhost:${port}`);
+});
 
